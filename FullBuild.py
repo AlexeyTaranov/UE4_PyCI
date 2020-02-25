8eb3cc8ad79c7c19459f0c6bf0d594906d356c0f
Alexey Taranov <alexey.taranov0@gmail.com>
2020-02-25 21:45:29 +0300

    Added select configuration, platform

--- FullBuild.py
+++ FullBuild.py
@@ -3,33 +3,37 @@ import ue4paths
 import projectParser
 
 
-def checkCode(code):
-    if code != 0:
-        print(code)
-        exit()
-
-
 def start_proc(args):
     proc = subprocess.Popen(args)
     code = proc.wait()
-    checkCode(code)
+    if code != 0:
+        print(code)
+        exit(1)
 
 
 project = projectParser.BuildProjectInfo()
+platform = project.platform()
+configuration = project.configuration()
+platform_name = '-platform=' + platform.name
+configuration_name = '-configuration=' + configuration.name
 
 uproject = '-project=' + projectParser.BuildProjectInfo().uproject()
-build_args = [ue4paths.ubt(), uproject, '-projectfiles', '-game', '-rocket', 'progress']
+build_platforms = '-Platforms=' + platform.name
+build_args = [ue4paths.ubt(), uproject, '-projectfiles', '-game', '-rocket', 'progress', build_platforms,
+              '-noSolutionSuffix']
 start_proc(build_args)
 
-compile_args = [ue4paths.MSVC, project.solution(), r'/t:build', '/p:Platform=Win64;verbosity=diagnostic']
+platform_name_verb = '/p:Platform=' + platform.name
+configuration_name_verb = '/p:Configuration=' + configuration.name
+compile_args = [ue4paths.MSVC, project.solution(), r'/t:build', '-m', configuration_name_verb, platform_name_verb]
 start_proc(compile_args)
 
 build = '-archivedirectory=' + project.buildPath()
-build_args = [ue4paths.uat(), 'BuildCookRun', uproject, '-noP4', '-platform=Win64', '-clientconfig=Development',
-              '-cook', '-allmaps', '-build', '-stage', '-pak', '-archive', build]
-start_proc(build_args)
-build_proc = subprocess.Popen(build_args)
 
-cook_args = [ue4paths.uat(), 'BuildCookRun', uproject, '-noP4', '-platform=Win64', '-clientconfig=Development',
-             '-cook', '-allmaps', '-NoCompile', '-stage', '-pak', '-archive', build]
+build_args = [ue4paths.uat(), 'BuildCookRun', uproject, '-noP4', '-cook', '-allmaps', '-build',
+              '-stage', '-pak', '-archive', build, platform_name, configuration_name]
+start_proc(build_args)
+
+cook_args = [ue4paths.uat(), 'BuildCookRun', uproject, '-noP4', '-cook', '-allmaps', '-NoCompile', '-stage', '-pak',
+             '-archive', build, platform_name]
 start_proc(cook_args)
--- projectParser.py
+++ projectParser.py
@@ -1,22 +1,56 @@
 import argparse
 from os import path
+from enum import Enum
 
 
 class BuildProjectInfo:
     def __init__(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('-projectPath', type=str, dest='projectPath')
-        parser.add_argument('-projectName', type=str, dest='name')
+        parser.add_argument('-projectName', type=str, dest='projectName')
         parser.add_argument('-buildPath', type=str, dest='buildPath')
-        parser.add_argument('-buildType', type=str, dest='type')
+        parser.add_argument('-configuration', type=int, dest='configuration')
         parser.add_argument('-platform', type=str, dest='platform')
         self.args = parser.parse_args()
+        self.checkAttr('projectPath')
+        self.checkAttr('projectName')
+
+    def checkAttr(self, atrname):
+        if hasattr(self, atrname):
+            print('Need attribute -' + atrname)
+            exit(1)
 
     def solution(self):
-        return path.join(self.args.projectPath, self.args.name) + '.sln'
+        return path.join(self.args.projectPath, self.args.projectName) + '_' + self.platform().name + '.sln'
 
     def uproject(self):
-        return path.join(self.args.projectPath, self.args.name) + '.uproject'
+        return path.join(self.args.projectPath, self.args.projectName) + '.uproject'
 
     def buildPath(self):
+        if self.args.buildPath is None:
+            return r'C:\Builds'
         return self.args.buildPath
+
+    def platform(self):
+        if self.args.platform is None:
+            return Platform.Win64
+        if self.args.platform == 'Android':
+            return Platform.Android
+        elif self.args.platform == 'Win64':
+            return Platform.Win64
+
+    def configuration(self):
+        if self.args.configuration is None:
+            return Configuration.Shipping
+        return Configuration(self.args.configuration)
+
+
+class Platform(Enum):
+    Win64 = 0
+    Android = 1
+
+
+class Configuration(Enum):
+    DebugGame = 0
+    Development = 1
+    Shipping = 2
